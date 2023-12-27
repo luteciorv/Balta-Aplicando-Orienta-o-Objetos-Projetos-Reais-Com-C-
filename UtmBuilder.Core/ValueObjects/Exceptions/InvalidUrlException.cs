6da287cbf4642b626e4062dfd7d14a59bb2297fc
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-27 10:56:23 -0300

    Adição de summary na classe Url e Exceções personalizadas

---
+++ UtmBuilder.Core/ValueObjects/Exceptions/InvalidUrlException.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+
+namespace UtmBuilder.Core.ValueObjects.Exceptions;
+
+public class InvalidUrlException : Exception
+{
+    private const string UrlRegexPattern = @"^(http|https):\/\/[^\s\/$.?#].[^\s]*$";
+
+    public InvalidUrlException(string message = "Formato da Url inválido") : base(message)
+    { }
+
+    public static void ThrowIfInvalid(string address)
+    {
+        if(string.IsNullOrEmpty(address))
+            throw new InvalidUrlException("Url vazia");
+
+        if (!Regex.IsMatch(address, UrlRegexPattern))
+            throw new InvalidUrlException();
+    }
+}
--- UtmBuilder.Core/ValueObjects/Url.cs
+++ UtmBuilder.Core/ValueObjects/Url.cs
@@ -1,10 +1,20 @@
-﻿namespace UtmBuilder.Core.ValueObjects;
+﻿using System.Text.RegularExpressions;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.ValueObjects;
 
 public class Url : ValueObject
 {
+
+    /// <summary>
+    /// Cria uma nova URL
+    /// </summary>
+    /// <param name="address">Endereço da URL (link do website)</param>
     public Url(string address)
     {
         Address = address;
+
+        InvalidUrlException.ThrowIfInvalid(address);
     }
 
     public string Address { get; }
