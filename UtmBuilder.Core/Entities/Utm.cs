2350b0225964fe07902d4189e4d111b2d8c841d2
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-27 16:22:52 -0300

    Utilização de Implic Operators de string para Utm

--- UtmBuilder.Core/Entities/Utm.cs
+++ UtmBuilder.Core/Entities/Utm.cs
@@ -1,5 +1,6 @@
 ﻿using UtmBuilder.Core.Extensions;
 using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
 
 namespace UtmBuilder.Core.Entities;
 
@@ -16,6 +17,33 @@ public class Utm
 
     public static implicit operator string(Utm utm) 
         => utm.ToString();
+
+    public static implicit operator Utm(string link)
+    {
+        InvalidUrlException.ThrowIfInvalid(link);
+
+        var url = new Url(link);
+        var segments = url.Address.Split("?");
+
+        if (segments.Length == 1)
+            throw new InvalidUrlException("Nenhum segmento foi fornecido");
+
+        var pars = segments[1].Split("&");
+
+        var source = pars.Where(p => p.StartsWith("utm_source")).FirstOrDefault("").Split("=")[1]; 
+        var medium = pars.Where(p => p.StartsWith("utm_medium")).FirstOrDefault("").Split("=")[1];
+        var name = pars.Where(p => p.StartsWith("utm_name")).FirstOrDefault("").Split("=")[1];  
+        
+        var id = pars.Where(p => p.StartsWith("utm_id")).FirstOrDefault("").Split("=")[1];  
+        var term = pars.Where(p => p.StartsWith("utm_term")).FirstOrDefault("").Split("=")[1];  
+        var content = pars.Where(p => p.StartsWith("utm_content")).FirstOrDefault("").Split("=")[1];
+
+        var newUrl = new Url(segments[0]);
+        var campaign = new Campaign(source, medium, name, id, term, content);
+        var utm = new Utm(newUrl, campaign);
+
+        return utm;
+    }
 
     public override string ToString()
     {
