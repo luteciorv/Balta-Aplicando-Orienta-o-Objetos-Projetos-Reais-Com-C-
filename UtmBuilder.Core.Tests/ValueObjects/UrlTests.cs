90686fba27268ee15398c4f3cc45110eb0f04d34
Luís Felipe <luisrodrigues@bne.com.br>
2024-01-03 09:55:37 -0300

    Criação dos testes para a Url

---
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -0,0 +1,25 @@
+﻿using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.Tests.ValueObjects;
+
+[TestClass]
+public class UrlTests
+{
+    private const string InvalidUrl = "banana";
+    private const string ValidUrl = "https://balta.io";
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidUrlException))]
+    public void Dado_uma_url_invalida_deve_levantar_uma_excecao()
+    {
+        new Url(InvalidUrl);
+    }
+
+    [TestMethod]
+    public void Dado_uma_url_valida_nao_deve_levantar_uma_excecao()
+    {
+        new Url(ValidUrl);
+        Assert.IsTrue(true);
+    }
+}
